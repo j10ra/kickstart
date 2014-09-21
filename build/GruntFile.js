b57c293a509b3945092595535e9f2a979a7be84d
j10ra <j10ra@yahoo.com>
2014-09-21 16:50:56 +0800

    Removing Require.js

--- build/GruntFile.js
+++ build/GruntFile.js
@@ -7,20 +7,24 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         sass: {
+            // task
             dist: {
                 files: {
-                    '../css/main.css': 'assets/css/main.scss' // 'destination': 'source'
+                    '../css/main.css': 'assets/css/main.scss'     // 'destination': 'source'
                 },
                 options: {
-                    outputStyle: 'compressed'
+                    style: 'compressed',
+                    banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n/* Author: Jetz Alipalo */'
                 }
             },
             dev: {
                 options: {
-                    sourceMap: true
+                    sourcemap: true,
+                    style:'compact',
+                    banner: '/* <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n/* Author: Jetz Alipalo */\n'
                 },
                 files: {
-                    '../css/main.css': 'assets/css/main.scss' // dictionary of render options
+                    '../css/main.css': 'assets/css/main.scss'
                 }
             }
         },
@@ -34,8 +38,9 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'assets/js/',
-                    src: '*.js',
-                    dest: '../js/app/'
+                    src: ['*.js', '**/*.js'],
+                    dest: '../js/',
+                    ext: '.min.js'
                 }]
             },
             dev: {
@@ -45,28 +50,112 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'assets/js/',
-                    src: '*.js',
-                    dest: '../js/app/'
+                    src: ['*.js', '**/*.js'],
+                    dest: '../js/',
+                    ext: '.min.js'
                 }]
             }
+        },
+
+        jshint: {
+            options: {
+                "white": true,
+                "indent": 4,
+                "onevar": true,
+                "onecase": false,
+                "immed": true,
+                "debug": false,
+                "evil": false,
+                "strict": false,
+                "multistr": false,
+                "laxbreak": false,
+                "globalstrict": false,
+                "supernew": false,
+                "laxcomma": false,
+                "asi": false,
+                "es5": false,
+                "scripturl": false,
+                "withstmt": false,
+                "bitwise": true,
+                "eqeqeq": true,
+                "shadow": false,
+                "expr": false,
+                "noarg": true,
+                "newcap": true,
+                "forin": true,
+                "regexdash": false,
+                "node": false,
+                "eqnull": false,
+                "browser": true,
+                "iterator": false,
+                "undef": true,
+                "latedef": true,
+                "nonstandard": false,
+                "trailing": true,
+                "jquery": true,
+                "loopfunc": false,
+                "boss": false,
+                "nonew": true,
+                "funcscope": false,
+                "regexp": true,
+                "lastsemic": false,
+                "smarttabs": true,
+                "devel": false,
+                "esnext": false,
+                "sub": false,
+                "curly": true,
+                "prototypejs": false,
+                "proto": false,
+                "plusplus": false,
+                "noempty": false,
+                "predef": [
+                    /* browser global methods */
+                    "confirm",
+                    "alert",
+                    /* general libraries and tools */
+                    "_gaq",
+                    "jRespond",
+                    "Mustache",
+                    "_",
+                    "angular",
+                    "define",
+                    "require",
+                    "Backbone",
+                    /* jasmine */
+                    "jasmine",
+                    "spyOn",
+                    "it",
+                    "console",
+                    "describe",
+                    "expect",
+                    "beforeEach",
+                    "waits",
+                    "waitsFor",
+                    "runs",
+                    "google",
+                    "MarkerClusterer",
+                    "InfoBubble",
+                    "Label"
+                ]
+            },
+            all: ['assets/js/app/**/*.js']
         },
 
         watch: {
             css: {
                 files: ['assets/css/*.scss'],
-                options: {
-                    livereload: true
-                },
                 tasks: ['sass:dev']
             },
 
             scripts: {
-                files: ['assets/js/*.js'],
+                files: ['*.js', '**/*.js'],
                 options: {
                     spawn: false
                 },
-                tasks: ['uglify:dev']
+                tasks: ['newer:uglify:dev','newer:jshint']
             }
+
+
         }
 
     });
--- build/assets/js/app/main.js
+++ build/assets/js/app/main.js
@@ -8,22 +8,12 @@ website.core = (function () {
     fn.page = {
         init: function () {
             console.log('app script: Loaded');
-
-            var hello = 20;
-            if(hello === 1){
-                alert(hello);
-            }
         }
     };
 
     return {
         run: function () {
             fn.page.init();
-            var hello = 0;
-
-            if(hello === 2) {
-                alert('sdasd');
-            }
         }
     };
 
